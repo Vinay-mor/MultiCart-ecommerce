e06b24db4eb7f5a7caf7347c49b39dd3c6884801
Vinay-mor <vinaymor2005@gmail.com>
2025-12-14 16:58:03 +0530

    home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,9 @@ import {Input} from "@/components/ui/input";
 import {Progress} from "@/components/ui/progress";
 import {Textarea} from "@/components/ui/textarea";
 import {Checkbox} from "@/components/ui/checkbox";
+
 export default function Home() {
+
   return (
     <div className="p-4">
     <div className="flex flex-col gap-y-4">
