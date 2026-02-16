7ecf70de0d0d5f75d0416d1de954a2d3101d179f
Vinay-mor <vinaymor2005@gmail.com>
2026-02-16 21:33:42 +0530

    Subdomain Rewrites

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 export function generateTenantURL(tenantSlug: string) {
-  return `/tenants/${tenantSlug}`;
+  if(process.env.NODE_ENV==="development"){
+     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
+  }
+  const protocol="https";
+  const domain=process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+  return `${protocol}://${tenantSlug}.${domain}`;
 }
 export function formatCurrency(value: number | string) {
   return new Intl.NumberFormat("en-US", {
