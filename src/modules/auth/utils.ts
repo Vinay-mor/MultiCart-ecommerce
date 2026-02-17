3b97c9aaa94baa5032b2c4c7f449a1239331cfa1
Vinay-mor <vinaymor2005@gmail.com>
2026-02-17 11:56:26 +0530

    Cookie-Bugs

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -1,17 +1,21 @@
 import { cookies as getCookies } from "next/headers";
-interface Props{
-    prefix:string;
-    value:string;
+interface Props {
+    prefix: string;
+    value: string;
 }
-export const generateAuthCookie = async ({prefix,value}:Props) => {
+export const generateAuthCookie = async ({ prefix, value }: Props) => {
     const cookies = await getCookies();
     cookies.set({
-        name:`${prefix}-token`,
+        name: `${prefix}-token`,
         value: value,
         httpOnly: true,
         path: "/",
-        sameSite:"none",
-        domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-        secure:process.env.NODE_ENV==="production",
+        //This enables the cookie auth on localhost
+        //But it will not work with subdomains turend on
+        ...(process.env.NODE_ENV !== "development" && {
+            sameSite: "none",
+            domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+            secure: true,
+        }),
     });
 }
