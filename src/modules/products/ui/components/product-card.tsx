00fc231970edb206f87f80b867dc69f9ca5bc8fe
Vinay-mor <vinaymor2005@gmail.com>
2026-02-18 12:41:34 +0530

    product-cart fix

--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -27,7 +27,7 @@ export const ProductCard = ({
         router.push(generateTenantURL(tenantSlug))        
     }
     return (
-        <Link href={`${generateTenantURL(tenantSlug)}/products/${id}`}>
+        <Link prefetch href={`/tenants/${tenantSlug}/products/${id}`}>
             <div className="hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-shadow border rounded-md bg-white overflow-hidden h-full flex flex-col">
                 <div className="relative aspect-square">
                     <Image
